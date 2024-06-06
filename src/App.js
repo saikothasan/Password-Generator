8ae0dd32c68bfe15b1be9a0e0d0579bdaa95984e
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-06-07 02:24:17 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import PasswordGenerator from './PasswordGenerator'
+function App() {
+  return (
+    <>
+    <PasswordGenerator/>
+    </>
+  );
+}
+
+export default App;
